2cada3813c1fa9ccebce52800594b4243c15dffd
zjn <528416493@qq.com>
2019-11-19 11:16:32 +0800

    堆排序

---
+++ src/com/zjn/algorithm/HeapSort.java
@@ -0,0 +1,81 @@
+package com.zjn.algorithm;
+
+import com.zjn.algorithm.util.CommenUtil;
+
+/**
+ * HeapSort   用到了完全二叉树的部分性质
+ * 完全二叉树放按层次遍历顺序存放入一个一维数组中：
+ * 如果从下标从1开始存储，则编号为i的结点的主要关系为：
+ * 双亲：下取整 （i/2）       左孩子：2i           右孩子：2i+1
+ * <p>
+ * 如果从下标从0开始存储，则编号为i的结点的主要关系为：
+ * 双亲：下取整 （(i-1)/2）     左孩子：2i+1        右孩子：2i+2
+ *
+ * @author zjn
+ * @date 2019/11/19
+ **/
+public class HeapSort {
+
+    //声明全局变量，用于记录数组长度
+    private static int len;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void sort(int arrays[]) {
+        //构建一个最大堆
+        buildMaxHeap(arrays);
+
+        //循环将堆首位（最大值）与末位交换，然后再重新调整最大值
+        while (len > 0) {
+            CommenUtil.swap(arrays, 0, len - 1);
+            len--;
+            //排除末位插入的最大值，再次调整为最大堆
+            adjustHeap(arrays, 0);
+        }
+    }
+
+    /**
+     * 建立最大堆
+     *
+     * @param
+     * @return :
+     * @author : zjn
+     * @date : 2019/11/19
+     */
+    public static void buildMaxHeap(int[] arrays) {
+        //从最后一个非叶子节点开始向上构造最大堆
+        //完全二叉树如果是满二叉树，每一层都是其前面所有层个数总和+1；
+        //完全二叉树按层级把元素放入数组，最后一个非叶子节点下标为 (len-1)/2
+        for (int i = (len - 1) / 2; i >= 0; i--) {
+            adjustHeap(arrays, i);
+        }
+    }
+
+    /**
+     * 调整使之成为最大堆
+     *
+     * @param
+     * @return :
+     * @author : zjn
+     * @date : 2019/11/19
+     */
+    public static void adjustHeap(int[] arrays, int i) {
+        int maxIndex = i;
+        //如果有左子树，且左子树大于父节点，则将最大指针指向左子树
+        if (i * 2 + 1 < len && arrays[i * 2 + 1] > arrays[maxIndex]) {
+            maxIndex = i * 2 + 1;
+        }
+        //如果有右子树，且右子树大于父节点，则将最大指针指向右子树
+        if (i * 2 + 2 < len && arrays[i * 2 + 2] > arrays[maxIndex]) {
+            maxIndex = i * 2 + 2;
+        }
+        //如果父节点不是最大值，则将父节点与最大值交换，并且递归调整与父节点交换的位置
+        if (maxIndex != i) {
+            CommenUtil.swap(arrays, maxIndex, i);
+            //递归判断与父节点交换的元素，是否满足大于其子节点，再次调整
+            adjustHeap(arrays, maxIndex);
+        }
+    }
+}
--- src/com/zjn/algorithm/compare/Test.java
+++ src/com/zjn/algorithm/compare/Test.java
@@ -17,56 +17,64 @@ import com.zjn.algorithm.util.CommenUtil;
 public class Test {
     public static void main(String[] args) {
         int data[] = generateArray(10000);
-       // CommenUtil.display(data);
+        CommenUtil.display(data);
         //冒泡排序
         int[] maopao1 = data.clone();
         long start1 = CommenUtil.getCurTime();
         MaopaoSort.sort(maopao1);
-        CommenUtil.inputCostTime("冒泡1",start1);
-        //CommenUtil.display(maopao1);
+        CommenUtil.inputCostTime("冒泡1", start1);
+        CommenUtil.display(maopao1);
 
         int[] maopao2 = data.clone();
         long start2 = CommenUtil.getCurTime();
         MaopaoSort.sort2(maopao2);
-        CommenUtil.inputCostTime("冒泡2",start2);
-        //CommenUtil.display(maopao2);
+        CommenUtil.inputCostTime("冒泡2", start2);
+        CommenUtil.display(maopao2);
 
         int[] quick = data.clone();
         long start3 = CommenUtil.getCurTime();
-        QuickSort.sort(quick,0,quick.length-1);
-        CommenUtil.inputCostTime("快速",start3);
-       // CommenUtil.display(quick);
-        
+        QuickSort.sort(quick, 0, quick.length - 1);
+        CommenUtil.inputCostTime("快速", start3);
+        CommenUtil.display(quick);
+
         int[] select = data.clone();
         long start4 = CommenUtil.getCurTime();
         SelectSort.sort(select);
-        CommenUtil.inputCostTime("选择",start4);
-       // CommenUtil.display(quick);
-        
+        CommenUtil.inputCostTime("选择", start4);
+        CommenUtil.display(select);
+
         int[] insert = data.clone();
         long start5 = CommenUtil.getCurTime();
         InsertSort.sort(insert);
-        CommenUtil.inputCostTime("插入",start5);
-       // CommenUtil.display(quick);
+        CommenUtil.inputCostTime("插入", start5);
+        CommenUtil.display(insert);
+
+        int[] heap = data.clone();
+        long start6 = CommenUtil.getCurTime();
+        InsertSort.sort(heap);
+        CommenUtil.inputCostTime("堆", start6);
+        CommenUtil.display(heap);
 
 
     }
 
-    /** 生成测试数组
+    /**
+     * 生成测试数组
+     *
      * @param
      * @return :
      * @author : zjn
      * @date : 2019/11/18
      */
-    public static int[] generateArray(int sum){
+    public static int[] generateArray(int sum) {
         int[] array = new int[sum];
         //生成数组
-        for(int i=0;i<sum-1;i++){
-            array[i]=i+1;
+        for (int i = 0; i < sum - 1; i++) {
+            array[i] = i + 1;
         }
         //随机打乱
-        for (int i = 0; i < sum-1; i++) {
-            int r = i + new Random().nextInt(sum-1-i);     // between i and N-1
+        for (int i = 0; i < sum - 1; i++) {
+            int r = i + new Random().nextInt(sum - 1 - i);     // between i and N-1
             Integer temp = array[i];
             array[i] = array[r];
             array[r] = temp;
